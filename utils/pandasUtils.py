fa7797858a17a4f7ff1bc8d7d327655f16b9fb78
Valeria Ryndina <vryndina@solanteq.com>
2022-12-24 00:39:21 +0500

    added text type

--- utils/pandasUtils.py
+++ utils/pandasUtils.py
@@ -1,20 +1,48 @@
+import numpy as np
 import pandas as pd
+
+from utils.utils import get_image_filepaths, get_name_by_path
 
 image_column_name = 'image'
 surface_column_name = 'surface'
+text_type_column_name = 'text_type'
+result_column_name = 'result'
 csv_path = 'data/dt.csv'
 
 
-def init_dt():
-    return pd.DataFrame(columns=[image_column_name, surface_column_name])
+def init_main_dt():
+    array_ = np.empty([0, 3])
+    for filename in get_image_filepaths():
+        image = get_name_by_path(filename)
+        array_ = np.append(array_, [[image, image.split("-")[0], '']], axis=0)
+
+    dt = pd.DataFrame(array_[:, 1:3],
+                      columns=[surface_column_name, text_type_column_name],
+                      index=array_[:, 0])
+    dt.index.name = image_column_name
+
+    return dt
 
 
-def get_dt():
-    return pd.read_csv(csv_path, index_col=' ')
+def init_dt(dt, data_name):
+    return pd.DataFrame(columns=[data_name],
+                        index=dt.index)
 
 
-def save_dt(dt):
+def get_main_dt():
+    return pd.read_csv(csv_path, index_col=image_column_name)
+
+
+def get_dt(data_name):
+    return pd.read_csv('data/' + data_name + '.csv', index_col=image_column_name)
+
+
+def save_main_dt(dt):
     dt.to_csv(csv_path)
+
+
+def save_dt(dt, data_name):
+    dt.to_csv('data/' + data_name + '.csv')
 
 
 def fill_surface(dt):
